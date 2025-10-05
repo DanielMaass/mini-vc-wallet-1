93d63125f8f75bc9192421dc09c77821ad56d277
Daniel Maaß <daniel@vertragswerk.de>
2025-10-05 19:18:38 +0200

    remove unused type definitions

--- apps/client/src/lib/trpc.ts
+++ apps/client/src/lib/trpc.ts
@@ -1,5 +1,5 @@
 import { createTRPCReact } from '@mini-vc-wallet-1/trpc/client';
-import type { appRouter } from '../../../server/dist/trpc/router.js';
+import type { appRouter } from '@server/app';
 type AppRouter = typeof appRouter;
 
 export const trpc = createTRPCReact<AppRouter>();
