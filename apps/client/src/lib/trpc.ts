98a25be8f59f1c35f80da5e0efb8297dde27e167
Daniel Maaß <daniel@vertragswerk.de>
2025-10-06 11:30:37 +0200

    fix docker build

--- apps/client/src/lib/trpc.ts
+++ apps/client/src/lib/trpc.ts
@@ -1,5 +1,4 @@
 import { createTRPCReact } from '@mini-vc-wallet-1/trpc/client';
-import type { appRouter } from '@server/app';
-type AppRouter = typeof appRouter;
+import type { AppRouter } from '@server/app';
 
 export const trpc = createTRPCReact<AppRouter>();
